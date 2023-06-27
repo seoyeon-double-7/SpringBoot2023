bf5f649215c38ed7a6b26224ee8ee03307116dbc
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-06-27 09:58:48 +0900

    .

---
+++ springTest0627/src/main/java/kr/hs/study/springTest0627/SpringTest0627Application.java
@@ -0,0 +1,13 @@
+package kr.hs.study.springTest0627;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest0627Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTest0627Application.class, args);
+	}
+
+}
---
+++ springTest0627/src/test/java/kr/hs/study/springTest0627/SpringTest0627ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.study.springTest0627;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTest0627ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
