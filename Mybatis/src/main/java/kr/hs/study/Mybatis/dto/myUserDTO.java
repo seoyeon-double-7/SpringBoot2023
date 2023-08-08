af342cd921d2683aa086dd7065c85178ae6654ad
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-08-08 16:01:54 +0900

    게시판 insert까지 완료

--- Mybatis/src/main/java/kr/hs/study/Mybatis/dto/myUserDTO.java
+++ Mybatis/src/main/java/kr/hs/study/Mybatis/dto/myUserDTO.java
@@ -5,5 +5,10 @@ import lombok.Data;
 @Data
 public class myUserDTO {
     private String id;
+    private String name;
     private String pw;
+    private String email;
+    private String title;
+    private String content;
+    private String hit;
 }
